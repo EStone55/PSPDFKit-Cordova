e518d50e63d79e863554987671a239765920988b
EStone55 <estone@masterlibrary.com>
2020-06-08 15:58:01 -0400

    Update OpenAssetModalListener.java

--- src/android/java/com/pspdfkit/cordova/event/OpenAssetModalListener.java
+++ src/android/java/com/pspdfkit/cordova/event/OpenAssetModalListener.java
@@ -5,6 +5,7 @@ import com.pspdfkit.ui.toolbar.ContextualToolbar;
 import com.pspdfkit.cordova.event.EventDispatcher;
 import com.pspdfkit.ui.toolbar.AnnotationEditingToolbar;
 import com.pspdfkit.ui.toolbar.ContextualToolbarMenuItem;
+import com.pspdfkit.cordova.event.AnnotationSelectedListener;
 
 import android.R;
 import android.graphics.Color;
