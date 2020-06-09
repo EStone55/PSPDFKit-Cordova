060933fb9d072b5548262c3d3b292acda48a43d2
EStone55 <estone@masterlibrary.com>
2020-06-09 15:33:37 -0400

    Update CordovaPdfActivity.java

--- src/android/java/com/pspdfkit/cordova/CordovaPdfActivity.java
+++ src/android/java/com/pspdfkit/cordova/CordovaPdfActivity.java
@@ -97,7 +97,7 @@ public class CordovaPdfActivity extends PdfActivity implements OnContextualToolb
       toolbar.setMenuItems(menuItems);
       try {
         JSONObject data = new JSONObject();
-        data.put("item", menuItems[0]);
+        data.put("item", menuItems.get(0));
         EventDispatcher.getInstance().sendEvent("onGenericEvent", data);
       } catch(JSONException ex) {
         // squash
